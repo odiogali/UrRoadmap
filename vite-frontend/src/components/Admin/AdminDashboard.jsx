e8bf582549900e5360661da139f654751424e941
stephaniesevilla <stephaniesevillaa@gmail.com>
2025-04-21 14:16:47 -0600

    forgot to commit the modals lol

--- vite-frontend/src/components/Admin/AdminDashboard.jsx
+++ vite-frontend/src/components/Admin/AdminDashboard.jsx
@@ -3,7 +3,7 @@ import AdminNav from "./AdminNav";
 import StudentView from "./StudentView";
 import CourseView from "./CourseView";
 import FacultyView from "./FacultyView";
-import Dashboard from "./Dashboard";
+//import Dashboard from "./Dashboard";
 
 function AdminDashboard() {
   return (
@@ -14,13 +14,17 @@ function AdminDashboard() {
         <Routes>
           <Route
             index
-            element={<Dashboard />}
+            element={
+              //<Dashboard />
+              //added this bc frontend wouldnt run. can resolve this once dashboard file is added.
+              <div>
+                <h1>Admin Home</h1>
+                <p>Dashboard component missing for now.</p>
+              </div>
+            }
           />
           <Route path="students" element={<StudentView />} />
-          <Route
-            path="faculty"
-            element={<FacultyView />}
-          />
+          <Route path="faculty" element={<FacultyView />} />
           <Route path="courses" element={<CourseView />} />
         </Routes>
       </div>
