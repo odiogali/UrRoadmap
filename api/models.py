13b310ad0dd7ccc88d648c42e0915ff5b6332abf
Odi Ogali <odi.ogali1380@gmail.com>
2025-04-21 14:26:34 -0600

    Initial database changes

--- api/models.py
+++ api/models.py
@@ -9,61 +9,109 @@ from django.db import models
 
 
 class AdminStaff(models.Model):
-    eid = models.IntegerField(db_column='EID', primary_key=True)  # Field name made lowercase.
-    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    salary = models.IntegerField(db_column='Salary', blank=True, null=True)  # Field name made lowercase.
-    dno = models.ForeignKey('Department', models.DO_NOTHING, db_column='Dno', blank=True, null=True)  # Field name made lowercase.
+    eid = models.IntegerField(db_column='EID', primary_key=True)  
+    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  
+    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  
+    salary = models.IntegerField(db_column='Salary', blank=True, null=True)  
+    dno = models.ForeignKey('Department', models.DO_NOTHING, db_column='Dno', blank=True, null=True)  
 
     class Meta:
         managed = False
         db_table = 'admin_staff'
 
 
-class Course(models.Model):
-    course_code = models.CharField(db_column='Course_code', primary_key=True, max_length=10)  # Field name made lowercase.
-    textbook_isbn = models.CharField(db_column='Textbook_ISBN', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    prof = models.ForeignKey('Professor', models.DO_NOTHING, db_column='Prof_id', blank=True, null=True)  # Field name made lowercase.
-    dno = models.ForeignKey('Department', models.DO_NOTHING, db_column='Dno', blank=True, null=True)  # Field name made lowercase.
+class Course(models.Model): # DONE
+    course_code = models.CharField(db_column='Course_code', primary_key=True, max_length=10)  
+    textbook_isbn = models.ForeignKey('Textbook', on_delete=models.SET_NULL, db_column='Textbook_ISBN', null=True)
+    prof = models.ForeignKey(
+        'Professor', # WARNING: Should remove the foreign key and refer to possible sections to show teacher(s)
+        on_delete=models.SET_DEFAULT, # course should not be deleted with a professor
+        db_column='Prof_id', 
+        default=1
+    )
+    dno = models.ForeignKey('Department', models.CASCADE, db_column='Dno')  # Course CANNOT exist without department
 
     class Meta:
         managed = False
         db_table = 'course'
 
 
-class DegreeProgram(models.Model):
-    prog_name = models.CharField(db_column='Prog_name', primary_key=True, max_length=50)  # Field name made lowercase.
-    specialization = models.CharField(db_column='Specialization', max_length=50, blank=True, null=True)  # Field name made lowercase.
+class DegreeProgram(models.Model): # DONE
+    prog_name = models.CharField(db_column='Prog_name', primary_key=True, max_length=50)
 
     class Meta:
         managed = False
         db_table = 'degree_program'
 
+class Specialization(models.Model): # DONE
+    sname = models.CharField(db_column='SName', max_length=50)
+    prog_name = models.ForeignKey(
+        'DegreeProgram',
+        on_delete=models.CASCADE,
+        db_column='Prog_name'
+    )
 
-class Department(models.Model):
-    dno = models.IntegerField(db_column='Dno', primary_key=True)  # Field name made lowercase.
-    dname = models.CharField(db_column='Dname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    manager_id = models.IntegerField(db_column='Manager_id', blank=True, null=True)  # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'specialization'
+        unique_together = (('prog_name', 'sname'),)
+
+    @property
+    def pk(self):
+        return (self.prog_name, self.sname)
+
+class Department(models.Model): # DONE
+    dno = models.AutoField(db_column='Dno', primary_key=True)
+    dname = models.CharField(db_column='Dname', max_length=50)  
+    manager_id = models.IntegerField(db_column='Manager_id', blank=True, null=True)  
 
     class Meta:
         managed = False
         db_table = 'department'
 
 
-class Graduate(models.Model):
-    student = models.OneToOneField('Student', models.DO_NOTHING, db_column='Student_id', primary_key=True)  # Field name made lowercase.
-    thesis_title = models.CharField(db_column='Thesis_title', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    research_area = models.CharField(db_column='Research_area', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    teaching = models.ForeignKey('TeachingStaff', models.DO_NOTHING, db_column='Teaching_id', blank=True, null=True)  # Field name made lowercase.
+class Student(models.Model): # DONE
+    student_id = models.IntegerField(db_column='Student_id', primary_key=True)  
+    fname = models.CharField(db_column='Fname', max_length=50)  
+    lname = models.CharField(db_column='Lname', max_length=50)  
+
+    class Meta:
+        managed = False
+        db_table = 'student'
+
+
+class Graduate(models.Model): # DONE
+    student = models.OneToOneField('Student', models.CASCADE, db_column='Student_id', primary_key=True)  
+    thesis_title = models.CharField(db_column='Thesis_title', max_length=100, null=True)  
+    research_area = models.CharField(db_column='Research_area', max_length=50)  
+    teaching = models.ForeignKey('TeachingStaff', models.SET_NULL, db_column='Teaching_id', null=True)  
 
     class Meta:
         managed = False
         db_table = 'graduate'
 
 
-class HasAsAntireq(models.Model):
-    antireq_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='Antireq_code')
-    course_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='Course_code', related_name='hasasantireq_course_code_set')
+class Undergraduate(models.Model): # DONE
+    student = models.OneToOneField(Student, models.CASCADE, db_column='Student_id', primary_key=True)  
+    credits_completed = models.IntegerField(db_column='Credits_completed', null=True)  
+
+    major = models.ForeignKey(DegreeProgram, models.SET_DEFAULT, db_column='Major', default="Computer Science")  
+    specialization = models.ForeignKey(Specialization, models.SET_NULL, db_column='Specialization', null=True)
+    minor = models.ForeignKey(DegreeProgram, models.SET_NULL, db_column='Minor', related_name='student_minor_set', null=True)  
+
+    class Meta:
+        managed = False
+        db_table = 'undergraduate'
+
+
+class HasAsAntireq(models.Model): # DONE
+    course_code = models.ForeignKey(
+        'Course',
+        models.CASCADE,
+        db_column='Course_code',
+        related_name='hasasantireq_course_code_set'
+    )
+    antireq_code = models.ForeignKey('Course', models.CASCADE, db_column='Antireq_code')
 
     class Meta:
         managed = False
@@ -72,12 +120,17 @@ class HasAsAntireq(models.Model):
 
     @property
     def pk(self):
-        return (self.antireq_code, self.course_code)
+        return (self.course_code, self.antireq_code)
 
 
-class HasAsPreq(models.Model):
-    prereq_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='Prereq_code')
-    course_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='Course_code', related_name='hasaspreq_course_code_set')
+class HasAsPreq(models.Model): # DONE
+    course_code = models.ForeignKey(
+        'Course',
+        models.CASCADE,
+        db_column='Course_code',
+        related_name='hasaspreq_course_code_set'
+    )
+    prereq_code = models.ForeignKey('Course', models.CASCADE, db_column='Prereq_code')
 
     class Meta:
         managed = False
@@ -86,7 +139,7 @@ class HasAsPreq(models.Model):
 
     @property
     def pk(self):
-        return (self.prereq_code, self.course_code)
+        return (self.course_code, self.prereq_code)
 
 class HasTaken(models.Model):
     course_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='Course_code')
@@ -101,36 +154,24 @@ class HasTaken(models.Model):
 
 
 class Professor(models.Model):
-    eid = models.IntegerField(db_column='EID', primary_key=True)  # Field name made lowercase.
-    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    salary = models.IntegerField(db_column='Salary', blank=True, null=True)  # Field name made lowercase.
-    dno = models.ForeignKey(Department, models.DO_NOTHING, db_column='Dno', blank=True, null=True)  # Field name made lowercase.
-    research_area = models.CharField(db_column='Research_area', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    teaching = models.ForeignKey('TeachingStaff', models.DO_NOTHING, db_column='Teaching_id', blank=True, null=True)  # Field name made lowercase.
+    eid = models.IntegerField(db_column='EID', primary_key=True)  
+    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  
+    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  
+    salary = models.IntegerField(db_column='Salary', blank=True, null=True)  
+    dno = models.ForeignKey(Department, models.DO_NOTHING, db_column='Dno', blank=True, null=True)  
+    research_area = models.CharField(db_column='Research_area', max_length=50, blank=True, null=True)  
+    teaching = models.ForeignKey('TeachingStaff', models.DO_NOTHING, db_column='Teaching_id', blank=True, null=True)  
 
     class Meta:
         managed = False
         db_table = 'professor'
 
 
-class Requires(models.Model):
-    id = models.AutoField(primary_key=True)
-    course_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='Course_code')
-    isbn = models.ForeignKey('Textbook', models.DO_NOTHING, db_column='ISBN')
-
-    class Meta:
-        managed = False
-        db_table = 'requires'
-        unique_together = (('course_code', 'isbn'),)
-
-
-class Section(models.Model):
-    id = models.AutoField(primary_key=True)
-    scourse_code = models.ForeignKey(Course, models.DO_NOTHING, db_column='SCourse_code')
+class Section(models.Model): # DONE
+    scourse_code = models.ForeignKey('Course', models.CASCADE, db_column='SCourse_code')
     s_id = models.IntegerField(db_column='S_ID')
-    semester = models.CharField(db_column='Semester', max_length=6, blank=True, null=True)
-    instructor = models.ForeignKey('TeachingStaff', models.DO_NOTHING, db_column='Instructor_id', blank=True, null=True)
+    semester = models.CharField(db_column='Semester', max_length=6, null=True)
+    instructor = models.ForeignKey('TeachingStaff', models.SET_NULL, db_column='Instructor_id', null=True)
 
     class Meta:
         managed = False
@@ -138,24 +179,12 @@ class Section(models.Model):
         unique_together = (('scourse_code', 's_id'),)
 
 
-class Student(models.Model):
-    student_id = models.IntegerField(db_column='Student_id', primary_key=True)  # Field name made lowercase.
-    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    major = models.ForeignKey(DegreeProgram, models.DO_NOTHING, db_column='Major', blank=True, null=True)  # Field name made lowercase.
-    minor = models.ForeignKey(DegreeProgram, models.DO_NOTHING, db_column='Minor', related_name='student_minor_set', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'student'
-
-
 class SupportStaff(models.Model):
-    eid = models.IntegerField(db_column='EID', primary_key=True)  # Field name made lowercase.
-    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    salary = models.IntegerField(db_column='Salary', blank=True, null=True)  # Field name made lowercase.
-    dno = models.IntegerField(db_column='Dno', blank=True, null=True)  # Field name made lowercase.
+    eid = models.IntegerField(db_column='EID', primary_key=True)  
+    fname = models.CharField(db_column='Fname', max_length=50, blank=True, null=True)  
+    lname = models.CharField(db_column='Lname', max_length=50, blank=True, null=True)  
+    salary = models.IntegerField(db_column='Salary', blank=True, null=True)  
+    dno = models.IntegerField(db_column='Dno', blank=True, null=True)  
 
     class Meta:
         managed = False
@@ -163,31 +192,22 @@ class SupportStaff(models.Model):
 
 
 class TeachingStaff(models.Model):
-    teaching_id = models.IntegerField(db_column='Teaching_ID', primary_key=True)  # Field name made lowercase.
+    teaching_id = models.IntegerField(db_column='Teaching_ID', primary_key=True)  
 
     class Meta:
         managed = False
-        db_table = 'teaching_staff'
+        db_table = 'teaching_staff' 
 
 
-class Textbook(models.Model):
-    isbn = models.CharField(db_column='ISBN', primary_key=True, max_length=20)  # Field name made lowercase.
-    title = models.CharField(db_column='Title', max_length=200, blank=True, null=True)  # Field name made lowercase.
-    edition_no = models.IntegerField(db_column='Edition_no', blank=True, null=True)  # Field name made lowercase.
-    price = models.FloatField(db_column='Price', blank=True, null=True)  # Field name made lowercase.
+class Textbook(models.Model): # DONE
+    isbn = models.CharField(db_column='ISBN', primary_key=True, max_length=13)  
+    title = models.CharField(db_column='Title', max_length=200, blank=True)  
+    edition_no = models.IntegerField(db_column='Edition_no', null=True)  
+    price = models.FloatField(db_column='Price', null=True)  
 
     class Meta:
         managed = False
         db_table = 'textbook'
-
-
-class Undergraduate(models.Model):
-    student = models.OneToOneField(Student, models.DO_NOTHING, db_column='Student_id', primary_key=True)  # Field name made lowercase.
-    credits_completed = models.IntegerField(db_column='Credits_completed', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'undergraduate'
 
 
 class WorksFor(models.Model):
