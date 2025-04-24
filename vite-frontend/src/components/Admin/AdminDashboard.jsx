c12b3ba8e11914ef9a8523692bff8477fb637dc0
Saif F <saif.l.farag@gmail.com>
2025-04-24 16:26:02 -0600

    Small fix

--- vite-frontend/src/components/Admin/AdminDashboard.jsx
+++ vite-frontend/src/components/Admin/AdminDashboard.jsx
@@ -1,27 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import AdminNav from "./AdminNav";
-import StudentView from "./StudentView";
-import CourseView from "./CourseView";
-import FacultyView from "./FacultyView";
-import Dashboard from "./Dashboard/Dashboard";
 
 function AdminDashboard() {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <AdminNav />
-
       <div style={{ flex: 1, padding: "1rem" }}>
-        <Routes>
-          <Route
-            index
-            element={
-              <Dashboard />
-            }
-          />
-          <Route path="students" element={<StudentView />} />
-          <Route path="faculty" element={<FacultyView />} />
-          <Route path="courses" element={<CourseView />} />
-        </Routes>
+        <Outlet />
       </div>
     </div>
   );
